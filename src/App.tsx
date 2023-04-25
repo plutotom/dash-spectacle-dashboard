f231617d69e353c6a055cec8ee5423db32eabe38
isaiah <isaiahd.proctor@gmail.com>
2023-04-25 16:22:59 -0500

    made a new branch only for this full screen image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import SideNav from "./components/SideNav";
 import Dashboard from "./components/Dashboard";
 import "./styles/app.css";
-
+import cupule from "./assets/looking-together.jpg";
 function App() {
   return (
     <body>
       <div className="main-container">
         {/* <SideNav /> */}
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <div
+          className="imageFull"
+          style={{ backgroundImage: `url(${cupule})` }}
+        ></div>
       </div>
     </body>
   );
