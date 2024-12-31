a4a74f77c259fe50e9706b46b88cac030a45ea99
Isaiah Proctor <isaiahd.proctor@gmail.com>
2024-12-31 11:37:53 -0600

    added update-dashboard.sh script

---
+++ update-dashboard.sh
@@ -0,0 +1,15 @@
+git pull origin main
+
+docker compose build
+docker compose up -d
+
+docker compose exec -t laravel.test composer install
+docker compose exec -t laravel.test php artisan migrate
+docker compose exec -t laravel.test php artisan db:seed
+docker compose exec -t laravel.test artisan key:generate
+docker compose exec -t laravel.test artisan optimize
+docker compose exec -t laravel.test php artisan config: cache
+docker compose exec -t laravel.test php artisan route:cache
+docker compose exec -t laravel.test php artisan view:cache
+
+echo "done"
