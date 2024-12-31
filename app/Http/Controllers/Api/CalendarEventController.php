83eb3000ef56ae3a05a701fceb1398e1f663f1d5
Isaiah Proctor <isaiahd.proctor@gmail.com>
2024-12-31 12:14:42 -0600

    updated calendar missing cache

--- app/Http/Controllers/Api/CalendarEventController.php
+++ app/Http/Controllers/Api/CalendarEventController.php
@@ -3,31 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\CalendarEventResource;
-use Spatie\GoogleCalendar\Event;
+use App\Http\Resources\CalendarEventCollection;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\Cache;
-use App\Http\Resources\CalendarEventCollection;
+use Spatie\GoogleCalendar\Event;
 
 class CalendarEventController extends Controller
 {
-
     public function index()
     {
         try {
-            $cacheKey = 'calendar_events_' . Carbon::now()->format('Y-m-d_H-i-s');
+            $cacheKey = 'calendar_events_'.Carbon::now()->format('Y-m-d');
             $events = Cache::remember($cacheKey, now()->addMinutes(15), function () {
                 $startDate = Carbon::now();
                 $endDate = Carbon::now()->addMonth();
+
                 return Event::get($startDate, $endDate);
             });
-            
+
             return new CalendarEventCollection($events);
 
         } catch (Exception $e) {
-            \Log::error('Calendar Error: ' . $e->getMessage());
-            return response()->json(['error' => 'Failed to fetch calendar events' . $e->getMessage(), 'error_code' => $e->getCode()], 500);
+            \Log::error('Calendar Error: '.$e->getMessage());
+
+            return response()->json(['error' => 'Failed to fetch calendar events'.$e->getMessage(), 'error_code' => $e->getCode()], 500);
         }
     }
 }
