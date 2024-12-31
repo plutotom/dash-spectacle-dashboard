3eb63acb003bc21e10cf4bdff6f8a4266eff2b51
Isaiah Proctor <isaiahd.proctor@gmail.com>
2024-12-31 11:39:28 -0600

    fixed misspelling

--- update-dashboard.sh
+++ update-dashboard.sh
@@ -8,7 +8,7 @@ docker compose exec -t laravel.test php artisan migrate
 docker compose exec -t laravel.test php artisan db:seed
 docker compose exec -t laravel.test artisan key:generate
 docker compose exec -t laravel.test artisan optimize
-docker compose exec -t laravel.test php artisan config: cache
+docker compose exec -t laravel.test php artisan config:cache
 docker compose exec -t laravel.test php artisan route:cache
 docker compose exec -t laravel.test php artisan view:cache
 
