45a11176ed19d8cee4b0ea84086298bd4fe955ad
Isaiah Proctor <isaiahd.proctor@gmail.com>
2025-05-24 20:03:44 -0500

    built files

---
+++ public/build/assets/InputLabel-kVQ0iKrg.js
@@ -0,0 +1,2 @@
+import{j as s}from"./app-TYR3tU8e.js";function n({value:t,className:e="",children:r,...a}){return s.jsx("label",{...a,className:"block text-sm font-medium text-gray-700 dark:text-gray-300 "+e,children:t||r})}export{n as I};
+//# sourceMappingURL=InputLabel-kVQ0iKrg.js.map
