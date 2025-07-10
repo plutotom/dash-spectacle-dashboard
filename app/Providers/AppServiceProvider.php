8ca5a412f4583eccf1ef737973c54851809bcabf
Isaiah Proctor <isaiahd.proctor@gmail.com>
2025-07-10 09:34:09 -0500

    added log viewer with login requiered

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        LogViewer::auth(function ($request) {
+            return
+            $request->user() &&
+            str_contains($request->user()->email, 'plutotom');
+        });
     }
 }
