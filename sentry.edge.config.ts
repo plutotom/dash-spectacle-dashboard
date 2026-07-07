6464d919445a1a87c37f5ebcc7dfc08d2d4abb4a
Isaiah Proctor <isaiahd.proctor@gmail.com>
2026-07-07 09:25:02 -0500

    added sentry v2

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,13 +1,21 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
-import { getSentryDsn, SENTRY_TRACES_SAMPLE_RATE } from "./sentry.shared";
+import { getSentryDsn } from "./sentry.shared";
 
-const dsn = getSentryDsn();
+Sentry.init({
+  dsn: getSentryDsn(),
 
-if (dsn) {
-  Sentry.init({
-    dsn,
-    environment: process.env.SENTRY_ENVIRONMENT ?? process.env.NODE_ENV,
-    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
-    enableLogs: true,
-  });
-}
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,14 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
-import { getSentryDsn, SENTRY_TRACES_SAMPLE_RATE } from "./sentry.shared";
 
-const dsn = getSentryDsn();
+Sentry.init({
+  dsn: "https://8da3ae9d743a509a7f2b562822c42abb@o4511096459886592.ingest.us.sentry.io/4511694441414656",
 
-if (dsn) {
-  Sentry.init({
-    dsn,
-    environment: process.env.SENTRY_ENVIRONMENT ?? process.env.NODE_ENV,
-    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
-    includeLocalVariables: true,
-    enableLogs: true,
-  });
-}
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
